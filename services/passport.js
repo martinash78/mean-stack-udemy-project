11be7514d917c2a7252cc95352ac79671b803aa3
martinash78 <martin.ashcroft1978@gmail.comj>
2019-09-18 08:31:58 +0100

    Set up client side auth.

--- services/passport.js
+++ services/passport.js
@@ -23,20 +23,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // Already have a user record
-          done(null, existingUser);
-        } else {
-          // Create a new record
-          new User({
-            googleId: profile.id
-          })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
